7100891d502b7120f3b1b46f5364dcc44f290c25
akbhagasra <abhagasra225@gmail.com>
2024-04-14 17:19:00 +0530

    updated video player

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -1,32 +1,64 @@
 import React from "react";
 import { useSocketContext } from "../SocketContext";
+import ReactPlayer from "react-player";
+import Webcam from "react-webcam";
 
 const VideoPlayer = () => {
-    const { name, myVideo, userVideo, stream, callAccepted, callEnded, call } =
+    const { name, myVideo, userVideo, stream, callAccepted, call } =
         useSocketContext();
+    const videoConstraints = {
+        width: 1280,
+        height: 720,
+        facingMode: "user",
+    };
+
     return (
         <div className="flex flex-col lg:flex-row mt-4 justify-center align-middle items-center">
             {stream && (
                 <div className="w-fit bg-slate-50 rounded-xl">
                     <p className="text-xl text-center">{name || "--"}</p>
-                    <video
+                    {/* <video
                         className="h-350px rounded-b-xl w-full"
                         ref={myVideo}
                         autoPlay
                         muted
                         playsInline
-                    ></video>
+                    ></video> */}
+                    {/* <ReactPlayer
+                        ref={myVideo}
+                        autoPlay
+                        muted
+                        className="h-350px rounded-b-xl w-full"
+                    /> */}
+                    <Webcam
+                        className="h-350px rounded-b-xl w-full"
+                        audio={false}
+                        height={720}
+                        ref={myVideo}
+                        screenshotFormat="image/jpeg"
+                        width={1280}
+                        videoConstraints={videoConstraints}
+                    />
                 </div>
             )}
-            {call.from && callAccepted && (
+            {callAccepted && (
                 <div className="w-fit bg-slate-50 rounded-xl lg:mt-0 lg:ml-4 mt-4">
-                    <p className="text-xl text-center">{call.from || "--"}</p>
-                    <video
+                    <p className="text-xl text-center">{call.name || "--"}</p>
+                    {/* <video
                         className="h-350px rounded-b-xl w-full"
                         ref={userVideo}
                         autoPlay
                         playsInline
-                    ></video>
+                    ></video> */}
+                    <Webcam
+                        className="h-350px rounded-b-xl w-full"
+                        audio={true}
+                        height={720}
+                        ref={userVideo}
+                        screenshotFormat="image/jpeg"
+                        width={1280}
+                        videoConstraints={videoConstraints}
+                    />
                 </div>
             )}
         </div>
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), vue()],
+    base: "/",
+    plugins: [react()],
+    preview: {
+        port: 8080,
+        strictPort: true,
+    },
+    server: {
+        port: 8080,
+        strictPort: true,
+        host: true,
+        origin: "http://0.0.0.0:8080",
+    },
 });
